191b55b219739db6e46706d09542a20d3f8d751b
Evo7Hu <evo7hu@gmail.com>
2024-05-04 21:57:06 +0800

    Site updated: 2024-05-04 21:57:06

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,1 @@
-var posts=["posts/4a17b156.html"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
+var posts=["posts/4a17b156.html"];function toRandomPost(){pjax.loadUrl("/"+posts[Math.floor(Math.random()*posts.length)])}
